b1815f3b44660874036c13a176ab25667525e4da
Archana Roy <archanaroy2003@gmail.com>
2026-07-30 18:47:53 +0530

    Add some line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Welcom to java track");
+        System.out.println("i already know how to build programing logic");
+        System.out.println("Now i learn how to express that logic using java:");
     }
 }
