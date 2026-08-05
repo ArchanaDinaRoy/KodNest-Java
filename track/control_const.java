06e999cf9ced1899be4d6bbc16f6748a4c45c057
Archana Roy <archanaroy2003@gmail.com>
2026-08-05 14:37:53 +0530

    control construct

---
+++ track/control_const.java
@@ -0,0 +1,25 @@
+package track;
+
+public class control_const {
+    public static void main(String[] args) {
+
+        int num = 20;
+
+        if (num > 50)
+
+        {
+
+            System.out.println("num is greater");
+
+        }
+
+        else
+
+        {
+
+            System.out.println("num is not greater");
+
+        }
+
+    }
+}
